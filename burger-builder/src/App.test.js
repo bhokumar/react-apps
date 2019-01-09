7108582101b527b2402c13dfe6548eaf8279e1bc
Bhoopendra Kumar <bhoopendra.akgec@gmail.com>
2019-01-09 23:43:34 +0530

    Adding .travis.yml and modifying test case

--- burger-builder/src/App.test.js
+++ burger-builder/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-xit('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<div>Hello</div>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
